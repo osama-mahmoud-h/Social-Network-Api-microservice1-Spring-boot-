afaeb6918435ec219ac87b96d678b49c03a42cf7
osama-mahmoud-h <om1408@gmail.com>
2023-06-24 13:53:21 +0300

    updating profile

--- src/main/java/com/example/server/services/impl/FilesStorageServiceImpl.java
+++ src/main/java/com/example/server/services/impl/FilesStorageServiceImpl.java
@@ -18,7 +18,7 @@ import java.util.stream.Stream;
 
 @Service
 public class FilesStorageServiceImpl implements FilesStorageService {
-    private final Path root = Paths.get("src/main/resources/static/uploads");
+    private final Path root = Paths.get("src/main/resources/static/");
     @Override
     public void init() {
         try {
