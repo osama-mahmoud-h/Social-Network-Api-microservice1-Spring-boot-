b3db5eb4edbcf073c88ed69dd1353602df91e90c
osama <osama.mahmoud.h9@gmail.com>
2025-02-07 18:20:58 +0200

    currently disable eureka as client

--- services/main-service/src/main/java/com/app/server/ServerApplication.java
+++ services/main-service/src/main/java/com/app/server/ServerApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
-//@EnableWebMvc
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ServerApplication {
 
 	public static void main(String[] args) {
