8af5392b3145974715a5777e2b8e96fb8428bf9a
osama <osama.mahmoud.h9@gmail.com>
2025-02-10 09:47:54 +0200

    setup elk stack

--- services/discovery-service/src/main/java/semsem/discovery/DiscoveryApplication.java
+++ services/discovery-service/src/main/java/semsem/discovery/DiscoveryApplication.java
@@ -7,8 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
-
+    //log
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DiscoveryApplication.class);
     public static void main(String[] args) {
+        log.info("Discovery service started");
         SpringApplication.run(DiscoveryApplication.class, args);
     }
 
--- services/main-service/src/main/java/com/app/server/ServerApplication.java
+++ services/main-service/src/main/java/com/app/server/ServerApplication.java
@@ -1,5 +1,7 @@
 package com.app.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,8 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //@EnableDiscoveryClient
 public class ServerApplication {
+	private static final Logger log = LoggerFactory.getLogger(ServerApplication.class);
 
 	public static void main(String[] args) {
     SpringApplication.run(ServerApplication.class, args);
+	log.warn("ServerApplication started successfully");
 	}
 }
