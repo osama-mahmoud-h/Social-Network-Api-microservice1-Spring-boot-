db30ab77662f21a56c5a84f96d9777054f86a99d
osama mahmoud <osama.mahmoud.h9@gmail.com>
2025-11-30 21:13:03 +0200

    add ELK logging configuration and enable Eureka discovery client for Docker profiles

--- services/chat-service/src/main/java/semsem/chatservice/ChatServiceApplication.java
+++ services/chat-service/src/main/java/semsem/chatservice/ChatServiceApplication.java
@@ -2,12 +2,14 @@ package semsem.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"semsem.chatservice.client", "com.app.shared.security.client"})
 @ComponentScan(basePackages = {"semsem.chatservice", "com.app.shared.security.filter", "com.app.shared.security.client"})
+@EnableDiscoveryClient
 public class ChatServiceApplication {
 
     public static void main(String[] args) {
--- services/search-service/src/main/java/semsem/searchservice/SearchServiceApplication.java
+++ services/search-service/src/main/java/semsem/searchservice/SearchServiceApplication.java
@@ -2,10 +2,12 @@ package semsem.searchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"com.app.shared.security.client"})
+@EnableDiscoveryClient
 public class SearchServiceApplication {
 
     public static void main(String[] args) {
